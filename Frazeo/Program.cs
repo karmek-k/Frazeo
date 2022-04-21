145b94166cf80775295e6700459f039a0f345c13
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-21 18:06:12 +0200

    Add boilerplate from Discord.Net docs

--- Frazeo/Program.cs
+++ Frazeo/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Frazeo
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static Task Main(string[] args) => new Program().AsyncMain();
+
+        public async Task AsyncMain()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, 
+                Environment.GetEnvironmentVariable("FRAZEO_TOKEN"));
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
