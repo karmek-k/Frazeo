033479a5d08424961f1b2efb18735f7b5f6c4218
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-21 19:30:47 +0200

    Create a Phrase model

---
+++ Frazeo/Database/Models/Phrase.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazeo.Database.Models
+{
+    internal class Phrase
+    {
+        public string Id { get; set; }
+
+        [BsonElement("phrase")]
+        public string PhraseText { get; set; }
+
+        public string[] Meanings { get; set; }
+    }
+}
