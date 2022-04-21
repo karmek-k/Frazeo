34f1e2206f17762434c7c9bbbe06c93cd3554479
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-21 19:15:09 +0200

    Set up dependency injection

--- Frazeo/Program.cs
+++ Frazeo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Frazeo.Bot;
+using Frazeo.Database;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Frazeo
 {
@@ -6,13 +8,25 @@ namespace Frazeo
     {
         public async Task AsyncMain()
         {
-            var bot = new FrazeoBot();
+            var bot = BuildServiceProvider().GetService<IBot>()!;
             var token = Environment.GetEnvironmentVariable("FRAZEO_TOKEN");
+
+            if (token is null)
+            {
+                throw new Exception("No bot token provided - use the FRAZEO_TOKEN env variable");
+            }
 
             await bot.Start(token);
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+        private static IServiceProvider BuildServiceProvider()
+        {
+            return new ServiceCollection()
+                .AddSingleton<IBot, FrazeoBot>()
+                .BuildServiceProvider();
         }
 
         public static Task Main(string[] args) => new Program().AsyncMain();
